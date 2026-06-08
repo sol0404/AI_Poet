a389f32af8a6cb5d47036d069656250a82fc3cd6
sol0404 <sol01024134256@gmail.com>
2026-06-08 21:42:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import streamlit as st
-
+from langchain_openai import ChatOpenAI
 
 chat_model = ChatOpenAI()
 
@@ -14,7 +14,7 @@ chat_model = ChatOpenAI()
 #result = chat_model.invoke(subject + "에 대한 시를 써줘.")
 #print(result.content)
 
-from langchain_openai import ChatOpenAI
+
 
 st.title("인공지능 시인")
 
